d3ed7d5a8c28ca15a25a0410463dd36863fc539e
droopyantidote <144815313+droopyantidote@users.noreply.github.com>
2024-03-12 09:40:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
-//your JS code here. If required.
+// Define the student object with a property called name
+var student = {
+    name: 'John'
+};
+
+// Add a method to the Object prototype called getKeys()
+Object.prototype.getKeys = function() {
+    // Use Object.keys() to get an array of keys in the object
+    return Object.keys(this);
+};
+
+// Test the getKeys() method
+console.log(student.getKeys()); // Output: ['name']
